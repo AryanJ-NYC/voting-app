f45f2d5e677f09820cfe1173832453af1f30409f
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-09-07 22:43:13 -0400

    Add Signup Modal

--- src/app/shared/nav/nav.module.ts
+++ src/app/shared/nav/nav.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginModalComponent } from './login-modal.component'
+import { LoginModalComponent } from './login-modal.component';
 import { NavComponent } from './nav.component';
+import { SignupModalComponent } from './signup-modal.component';
 
 @NgModule({
   imports: [ CommonModule ],
-  declarations: [ LoginModalComponent, NavComponent ],
+  declarations: [ LoginModalComponent, NavComponent, SignupModalComponent ],
   exports: [ NavComponent ]
 })
 
