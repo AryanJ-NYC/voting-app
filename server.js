3ba9c505bb7de46eee3fcdff5599ea59dfef781f
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-09-10 23:43:56 -0400

    Add body-parser; add error handler to server

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 'use strict';
 const express = require('express'),
       app = express(),
+      bodyParser = require('body-parser'),
       morgan = require('morgan'),
       path = require('path'),
       portNumber = process.env.PORT || process.argv[2] || 8080;
 
-app.use(express.static(__dirname)); 
+// basic setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+// views settings
+app.use(express.static(__dirname));
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: { message: err.message }});
+});
+
+// routes
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'src/index.html'));
 });
